8e9c998a16aae76e0ba99a528147a1bc05424e53
aveczzang <avec@dreamwiz.com>
2018-08-09 16:46:27 +0900

    RNN을 이용한 해석기(translator)

---
+++ translate-ex.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+char_arr = [c for c in 'SEPabcdefghijklmnopqrstuvwxyz단어나무놀이소녀키스사랑']
+num_dic = {n: i for i, n in enumerate(char_arr)}
+dic_len = len(num_dic)
+
+print('char_arr:\n', char_arr)
+print('num_dic:\n', num_dic)
+print('number of letters in our alphabet:', dic_len, '\n')
+
+# seq0 : wood, seq1 : 나무
+seq_data = [['wood', '나무'], ['love', '사랑']]
+
+def make_batch(seq_data):
+    input_batch = []
+    output_batch = []
+    target_batch = []
+
+    for seq in seq_data:
+        input = [num_dic[n] for n in seq[0]]
+        output = [num_dic[n] for n in ('S' + seq[1])]
+        target = [num_dic[n] for n in (seq[1] + 'E')]
+
+        print('word = ', seq)
+        print('input:', input)
+        print('output:', output)
+        print('target', target)
+
+        input_one = np.eye(dic_len)[input]
+        output_one = np.eye(dic_len)[output]
+
+        print('input_one\n', input_one)
+        print('output_one\n', output_one)
+        print()
+
+        input_batch.append(input_one)
+        output_batch.append(output_one)
+        target_batch.append(target)
+    print('=== final result ===')
+    print('input_batch:\n', input_batch)
+    print('output_batch:\n', output_batch)
+    print('target_batch:\n', target_batch)
+
+    return input_batch, output_batch, target_batch
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+input_batch, output_batch, target_batch = make_batch(seq_data)
+
+
---
+++ translate.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+char_arr = [c for c in 'SEPabcdefghijklmnopqrstuvwxyz단어나무놀이소녀키스사랑']
+num_dic = {n: i for i, n in enumerate(char_arr)}
+dic_len = len(num_dic)
+n_class = n_input = dic_len
+
+# seq0 : wood, seq1 : 나무
+seq_data = [['word', '단어'], ['wood', '나무'],
+            ['game', '놀이'], ['girl', '소녀'],
+            ['kiss', '키스'], ['love', '사랑']]
+
+# one-hot encoding function
+def make_batch(seq_data):
+    input_batch = []
+    output_batch = []
+    target_batch = []
+
+    for seq in seq_data:
+        input = [num_dic[n] for n in seq[0]]
+        output = [num_dic[n] for n in ('S' + seq[1])]
+        target = [num_dic[n] for n in (seq[1] + 'E')]
+
+        input_batch.append(np.eye(dic_len)[input])
+        output_batch.append(np.eye(dic_len)[output])
+        target_batch.append(target)
+
+    return input_batch, output_batch, target_batch
+
+learning_rate = 0.01
+n_hidden = 128
+total_epoch = 100
+
+# Seq2Seq RNN의 encoder, decoder
+# [batch size, time steps, input size]
+enc_input = tf.placeholder(tf.float32, [None, None, n_input])
+dec_input = tf.placeholder(tf.float32, [None, None, n_input])
+
+# output : [batch size, time steps]
+targets = tf.placeholder(tf.int64, [None, None])
+
+with tf.variable_scope('encode'):
+    enc_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+    enc_cell = tf.nn.rnn_cell.DropoutWrapper(enc_cell, output_keep_prob=0.5)
+    outputs, enc_states = tf.nn.dynamic_rnn(enc_cell, enc_input, dtype=tf.float32)
+
+with tf.variable_scope('decode'):
+    dec_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+    dec_cell = tf.nn.rnn_cell.DropoutWrapper(dec_cell, output_keep_prob=0.5)
+    outputs, dec_states = tf.nn.dynamic_rnn(dec_cell, dec_input, initial_state=enc_states, dtype=tf.float32)
+
+model = tf.layers.dense(outputs, n_class, activation=None)
+
+cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=model, labels=targets))
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+input_batch, output_batch, target_batch = make_batch(seq_data)
+
+for epoch in range(total_epoch):
+    _, loss = sess.run([optimizer, cost], feed_dict={enc_input: input_batch, dec_input: output_batch, targets: target_batch})
+    print('epoch: %04d' % epoch, 'cost: %.4f' % loss)
+
+def translate(word):
+    seq = [word, 'PP']
+
+    input_batch, output_batch, target_batch = make_batch([seq])
+    prediction = tf.argmax(model, 2)
+    result = sess.run(prediction, feed_dict={enc_input: input_batch, dec_input: output_batch, targets: target_batch})
+
+    decoded = [char_arr[i] for i in result[0]]
+    translated = decoded[0] + decoded[1]
+    return translated
+
+print('word ->', translate('word'))
+print('wodr ->', translate('wodr'))
+print('love ->', translate('love'))
+print('loev ->', translate('loev'))
+print('abcd ->', translate('abcd'))
+print('wide ->', translate('wide'))
+print('gate ->', translate('gate'))
+
+
